bd8c087288f252f4d6cc151569f2f9deb18862c7
Gerson <gersondinis@gmail.com>
2024-01-05 18:07:37 -0100

    fix: max width on site

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { countries } from 'constants/country';
 
 export const App = () => {
   return (
-    <Stack gap={1} sx={{minWidth: '20rem'}}>
+    <Stack gap={1} sx={{minWidth: '20rem', maxWidth: '25rem'}}>
       {countries.map((country) => (
         <CountryCard country={country} key={country} />
       ))}
